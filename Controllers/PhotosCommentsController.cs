a34ddf920ac22bdd60eabb9b20d946dad76e32ba
Bai027fu <merarynbfuentes@live.com>
2024-12-07 14:57:16 -0600

    Adición de comentarios

--- Controllers/PhotosCommentsController.cs
+++ Controllers/PhotosCommentsController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
@@ -86,3 +87,93 @@ namespace AdventureWorksWebApp.Controllers
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdventureWorksWebApp.Models;
+
+namespace AdventureWorksWebApp.Controllers
+{
+    [Authorize]
+    public class PhotosCommentsController : Controller
+    {
+        private AdventureWorks_DBEntities db = new AdventureWorks_DBEntities();
+
+        // GET: PhotosComments/New/5
+        public ActionResult New(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photo.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            Comment comment = new Comment();
+            comment.PhotoID = photo.PhotoID;
+            return View(comment);
+        }
+
+        // POST: PhotosComments/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CommentID,User,Subject,Body,PhotoID")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.User = User.Identity.Name;
+                Photo photo = db.Photo.Find(comment.PhotoID);
+                if(photo == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                photo.Comment.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Photos");
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // GET: PhotosComments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comment.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            if(comment.User != User.Identity.Name)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            db.Comment.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Photos");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+>>>>>>> e2e505fa148360275bc03b1ce7b1843943b08a2e
